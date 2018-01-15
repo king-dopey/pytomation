d7245deb392feed1378acc3f3d13c510288ebe54
Unknown <king.dopey.10111@gmail.com>
2018-01-14 19:18:27 -0800

    Gave Docker a config directory structure to ensure only pyto user has read access to it
    Generally you would have these files owned by root and not accessible by anyone else, especially things that hold passwords, keys, and encryption certificates.
    This allows you to configure by placing files the following structure :
    /
       Intances\ <--Your inteface files here
       Config\config.py <--Pytomation Config
       Config\openzwave.xml <-- OpenZwave options.xml (to store network key)
       ssl\ <-- Your ssl encryption certificates
    
    Then run using a comand like this:
    sudo docker run -p 192.168.249.59:443:8443 -v /etc/pytomation:/secured dheaps/pytomation:dev

---
+++ dockerentry.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Move contents of the secured mounted volume to the correct locaitons
+# and give the only the pyto user read-only access to them
+
+# Copy OpenZwave options.xml 
+if [ -e /secured/config/openzwave.xml ]
+then
+	cp /secured/config/openzwave.xml /etc/openzwave/options.xml
+	chown pyto:root /etc/openzwave/options.xml
+	chmod 400 /etc/openzwave/options.xml
+fi
+
+# Copy Pytomation config.py
+if [ -e /secured/config/config.py ]
+then
+	cp /secured/config/config.py /home/pytomation/pytomation/common
+	chown pyto:root /home/pytomation/pytomation/common/config.py
+	chmod 400 /home/pytomation/pytomation/common/config.py
+fi
+
+# Copy Instance files
+if [ -d /secured/instances ]
+then
+	cp /secured/instances/* /home/pytomation/instances
+	chown pyto:root /home/pytomation/instances/*
+	chmod 400 /home/pytomation/instances/*
+fi
+
+# Copy SSL certificates
+if [ -d /secured/ssl ]
+then
+	cp -R /secured/ssl /home/pytomation/ssl
+	chown pyto:root /home/pytomation/ssl/*
+	chmod 400 /home/pytomation/ssl/*
+fi
+
+# Now run Pytomation as the pyto user
+exec su - pyto -c "python3 /home/pytomation/pytomation.py"
